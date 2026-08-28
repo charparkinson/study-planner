2232100d56309e83b9b6ce2accfd3b86f24520ea
Charlize Parkinson <charparkinson17@icloud.com>
2026-08-28 19:50:53 +0100

    add task creation and deadline input

--- version1.py
+++ version1.py
@@ -1,9 +1,26 @@
 from datetime import datetime
 
-# current date in the format dd/mm/yy
+# display current date in the format dd/mm/yy
 current_date = datetime.now()
-print(current_date.strftime("%d %B %Y"))
+print(current_date.strftime('%d %B %Y'))
 
-# current time
+# display current time
 current_time = datetime.now()
-print(current_time.strftime("%X"))
+print(current_time.strftime('%X'))
+
+
+# allow user to create a task
+task_name = input("What is the name of your task?: ")
+print(f"I am now setting up {task_name}")
+
+# get user to confirm a date for deadline of task
+valid_date = True
+while valid_date == True:
+    task_due = input("When is your task due? Please enter the due date in the form of dd/mm/yyyy: ")
+    try:
+        task_due = datetime.strptime(task_due, '%d/%m/%Y')
+        valid_date = False
+    except:
+        print("Wrong input, please try again.")
+
+print(f"{task_name} is due on {task_due.strftime('%d/%m/%Y')}")
